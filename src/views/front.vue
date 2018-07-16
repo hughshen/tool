1634e24ef3f114802138d717dc770b72d022bfe7
hughshen <hughssss@outlook.com>
2018-07-16 21:38:54 +0800

    Add ip tool

--- src/views/front.vue
+++ src/views/front.vue
@@ -26,6 +26,15 @@
                 <el-input v-model="form.randomString"></el-input>
             </el-col>
         </el-form-item>
+        <el-form-item label="IP 地址">
+            <el-col :span="5">
+                <el-input v-model="form.ipAddress" :disabled="!fetchSupported()"></el-input>
+            </el-col>
+            <el-col :span="5" class="center">
+                <el-button type="primary" @click="fetchIpInfoCurrent" :disabled="!fetchSupported()">当前</el-button>
+                <el-button type="primary" @click="fetchIpInfoDetails" :disabled="!fetchSupported()">详细</el-button>
+            </el-col>
+        </el-form-item>
     </el-form>
 </template>
 
@@ -46,7 +55,8 @@ export default{
                 timestamp: '',
                 dateText: '',
                 randomLength: 16,
-                randomString: ''
+                randomString: '',
+                ipAddress: '',
             }
         }
     },
@@ -60,12 +70,59 @@ export default{
         },
         generateRandomString() {
             this.form.randomString = randomstring.generate(this.form.randomLength);
+        },
+        fetchSupported() {
+            return 'fetch' in window;
+        },
+        fetchIpInfoCurrent() {
+            fetch('https://api.ip.sb/geoip')
+                .then(resp => {
+                    return resp.json();
+                })
+                .then(json => {
+                    this.form.ipAddress = json.ip;
+                })
+                .catch(e => {
+                    console.log(e);
+                    this.$notify.error({title: '获取失败！', duration: 2000});
+                });
+        },
+        fetchIpInfoDetails() {
+            if (!this.form.ipAddress.trim().length) {
+                this.$notify.error({title: '请输入 IP 地址！', duration: 2000});
+                return;
+            }
+
+            fetch('https://api.ip.sb/geoip/' + this.form.ipAddress)
+                .then(resp => {
+                    return resp.json();
+                })
+                .then(json => {
+                    var message = [];
+                    for (var key in json) {
+                        if (json.hasOwnProperty(key)) {
+                            message.push(`<span class="bold">${key}:</span> ${json[key]}`);
+                        }
+                    }
+
+                    this.$alert(message.join('<br>'), '详细信息', {
+                        closeOnClickModal: true,
+                        dangerouslyUseHTMLString: true
+                    }).catch(e => {});
+                })
+                .catch(e => {
+                    console.log(e);
+                    this.$notify.error({title: '获取失败！', duration: 2000});
+                });
         }
     }
 }
 </script>
 
 <style>
+.bold {
+    font-weight: bold;
+}
 .center {
     text-align: center;
 }
